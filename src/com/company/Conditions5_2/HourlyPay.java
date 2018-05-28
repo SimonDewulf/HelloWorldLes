a1fa89315df55f9e142908b2b42ac49ef8256cb0
SimonDewulf <simon_dewulf@hotmail.com>
2018-05-28 15:41:51 +0200

    gittest

--- src/com/company/Conditions5_2/HourlyPay.java
+++ src/com/company/Conditions5_2/HourlyPay.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class HourlyPay {
     public static void main(String[] args) {
-
+//amagiewostekèrel
 
         double grossPayPerWeekEmployee1;
         double grossPayPerWeekEmployee2;
