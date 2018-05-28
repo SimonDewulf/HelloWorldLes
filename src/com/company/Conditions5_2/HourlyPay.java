bb541e84a662c6b0b06be6cd87a98b632397805a
SimonDewulf <simon_dewulf@hotmail.com>
2018-05-28 15:56:32 +0200

    Merge branch 'master' of https://github.com/SimonDewulf/HelloWorldLes
    # Conflicts:
    #	src/com/company/Conditions5_2/HourlyPay.java

--- src/com/company/Conditions5_2/HourlyPay.java
+++ src/com/company/Conditions5_2/HourlyPay.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class HourlyPay {
     public static void main(String[] args) {
-//amagiewostekèrel
 
         double grossPayPerWeekEmployee1;
         double grossPayPerWeekEmployee2;
