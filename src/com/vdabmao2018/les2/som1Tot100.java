7cfb10426096b226576112ee82af68bcc1aa3de9
SimonDewulf <simon_dewulf@hotmail.com>
2018-05-29 15:00:06 +0200

    Merge branch 'master' of https://github.com/SimonDewulf/HelloWorldLes
        # Conflicts:
        #	src/com/company/Conditions5_2/HourlyPay.java

---
+++ src/com/vdabmao2018/les2/som1Tot100.java
@@ -0,0 +1,13 @@
+package com.vdabmao2018.les2;
+
+public class som1Tot100 {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        for(int i = 1; i <= 100; i++){
+            x = x + i;
+        }
+        System.out.println(x);
+    }
+}
