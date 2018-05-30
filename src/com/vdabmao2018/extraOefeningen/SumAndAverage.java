54568938d75f53617e53310f2994f36c0c2d3ebc
SimonDewulf <simon_dewulf@hotmail.com>
2018-05-30 13:49:04 +0200

    woensdag oefeningen

---
+++ src/com/vdabmao2018/extraOefeningen/SumAndAverage.java
@@ -0,0 +1,23 @@
+package com.vdabmao2018.extraOefeningen;
+
+public class SumAndAverage {
+    public static void main(String[] args) {
+
+        double sum = 0;
+        double i = 1;
+
+
+        for(i = 1; i <= 100; i++){
+
+            sum = sum + i;
+        }
+
+        double average = sum/i;
+        System.out.println("the sum is: " + sum);
+        System.out.println("the average is: " + average);
+
+        //why isn't it bloody taking everything as double... literally everything is a double.
+
+
+    }
+}
